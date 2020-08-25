cb49cffd455d47a05bdcf8eb302f15d2995bca1b
Arnawa Juan <arnawajuan12@gmail.com>
2020-08-25 14:50:22 +0700

    master

---
+++ app/src/main/java/platformpbp/uajy/guided4_bundle/MainActivity.java
@@ -0,0 +1,14 @@
+package platformpbp.uajy.guided4_bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
